335b764ae6bcbed5a72cc35ee43d4c500feedd3c
dudds <duddscosta@gmail.com>
2026-04-08 13:18:53 -0300

    Adaptação da pesquisa sequencial com for para recursiva feita por mim

---
+++ ordenação/sequencialrecursivo.cpp
@@ -0,0 +1,28 @@
+u#include <iostream>
+using namespace std;
+
+void busca(int x, int *s, int i, int n);
+
+int main() {
+    int x, n = 10, lista[n], comeco = 0, fim = 9;
+    for (int i = 0; i < n; i++)
+        cin>>lista[i];
+    cout<<"Digite qual número procura: ";
+    cin>>x;
+    cout<<endl;
+    busca(x, lista, comeco, fim);
+
+    return 0;
+}
+
+void busca(int x, int *s, int i, int n){
+    if (i >= n){
+        cout<<"Valor não encontrado."<<endl;
+        return;
+    } 
+    if (s[i] == x){
+        cout<<"Resultado encontrado na posição: "<<i<<endl;
+        return;
+    }
+    busca(x, s, i+1, n);
+}
