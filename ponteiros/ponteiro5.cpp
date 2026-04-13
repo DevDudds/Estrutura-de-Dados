fa6c880b3dbbde99393366525dca5946b8a1b6c5
devdudds <duddscosta@gmail.com>
2026-04-13 20:36:44 -0300

    Att 13/04

--- ponteiros/ponteiro5.cpp
+++ ponteiros/ponteiro5.cpp
@@ -2,12 +2,15 @@
 #include <cstdlib>
 #include <cstring>
 using namespace std;
+
+
+
 int main(){
     char a[20], *s;
     cin.getline(a, 20);
     s = a;
-    for (int i = 0; i < strlen(a); i++)
-        cout << *(s+i);
+    //for (int i = 0; i < strlen(a); i++)
+    //    cout << *(s+i);
     cout << endl;
 
     return 0;
